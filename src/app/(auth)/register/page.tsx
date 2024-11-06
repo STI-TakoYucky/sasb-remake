3ab87137974ae45810f7b01fb5f17ab3ea646c12
Lucky Estrada <estrada.lnp@gmail.com>
2024-11-07 00:34:19 +0800

    changes

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -42,7 +42,7 @@ export default function Register() {
       setError(true);
     } else {
         try {
-          const res = await fetch("/api/register", {
+          const res = await fetch("https://sasb-remake.vercel.app/api/register", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
