ef15c6dc76a2444248a9c6b3cdaefed0c9614ce9
Lucky Estrada <estrada.lnp@gmail.com>
2025-01-05 21:41:58 +0800

    Bug Fix

--- src/app/(root)/(user)/page.tsx
+++ src/app/(root)/(user)/page.tsx
@@ -1,17 +1,33 @@
 'use client'
-
 import FeaturedPosts from '@/components/FeaturedPosts';
 //install daisyUI   npm i -D daisyui@latest
 import Post from '@/components/Post'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-
+// Use getServerSideProps to fetch data on every request
+// app/(root)/(user)/page.tsx
+  
 export default function Home() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = process.env.NEXT_PUBLIC_API_URL;
+      const res = await fetch(`${URL}/api/get-posts`);
+      const result = await res.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []); // The empty dependency array ensures this runs only once when the component mounts.
+
+  if (!data) return <div>Loading...</div>;
 
   return (
     <main className='py-20'>
         <FeaturedPosts></FeaturedPosts>
-        <Post></Post>
+        <Post posts={data}></Post>
     </main>
   );
 }
