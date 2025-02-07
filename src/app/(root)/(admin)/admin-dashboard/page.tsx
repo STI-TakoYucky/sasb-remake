7552fd45464ed91991610a2158e1273199c87d40
Lucky Estrada <estrada.lnp@gmail.com>
2025-02-08 01:39:18 +0800

    Major changes to the admin side

--- src/app/(root)/(admin)/admin-dashboard/page.tsx
+++ src/app/(root)/(admin)/admin-dashboard/page.tsx
@@ -1,21 +1,40 @@
 'use client'
 
-import { AdminNavbar, PostPageForm, PostList } from "@/components/adminComponents";
+import { Navbar } from "@/components";
+import { PostPageForm, PostList } from "@/components/adminComponents";
 import { useState } from "react";
 
 const AdminDashboard = () => {
-  const [navActive, setNavActive] = useState<{ link: string; "y-pos": number }>({
+  const [navActive, setNavActive] = useState<{ link: string; "x-pos": number; width: number }>({
       link: "Create Post",
-      "y-pos": 124 - 90,
+      "x-pos": 20,
+      width: 109
     });
 
-    
+    const NavigationHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+      const target = e.target as HTMLElement;
+      console.log(target.offsetWidth)
+      if (target.tagName === "LI") {
+        setNavActive({
+          link: target.innerHTML,
+          "x-pos": target.getBoundingClientRect().x,
+          width: target.offsetWidth
+        });
+      }
+    };
+
   return (
     <>
-      <AdminNavbar navActive={navActive} setNavActive={setNavActive}></AdminNavbar>
-      <main className="py-28">
-        <section className="global-mx">
-          <h1 className="text-3xl font-bold uppercase mb-6">Create post</h1>
+    <Navbar></Navbar>
+      <main className="py-28 global-mx">
+        <div className="mt-3 mb-5 flex items-center" onClick={NavigationHandler}>
+          <span className="bg-primary-200 absolute h-8 rounded-full transition-all duration-500" style={{ left: `${navActive["x-pos"]}px`, width: `${navActive["width"]}px`}}></span>
+          <ul className="navigation__container flex items-center gap-3">
+            <li>Create Post</li>
+            <li>Post List</li>
+          </ul>
+        </div>
+        <section className="">
           {
             navActive.link == "Create Post" && <PostPageForm></PostPageForm>
           }
--- src/components/adminComponents/PostList.tsx
+++ src/components/adminComponents/PostList.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function PostList() {
   return (
-    <div>PostList</div>
-  )
+    <div className="flex items-center justify-center bg-slate-100">
+      <div>
+        <img></img>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam velit
+          error, labore quae quam dignissimos asperiores nobis nostrum voluptas,
+          laudantium architecto quidem iure similique fugit mollitia optio quas
+          saepe libero.
+        </p>
+      </div>
+      <div className="flex items-center justify-center">
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+    </div>
+  );
 }
